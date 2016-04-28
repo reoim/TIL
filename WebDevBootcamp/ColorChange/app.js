34a5b77736dd01ba7a392ebcc520e44721a1a196
Dongmin (Reo) Lee <reoim10@gmail.com>
2016-04-28 22:51:01 +1000

    DOM manipulation
    2 different way of using EventListener to change background colour.

--- WebDevBootcamp/ColorChange/app.js
+++ WebDevBootcamp/ColorChange/app.js
@@ -1,8 +1,29 @@
 var body = document.querySelector("body");
 var button = document.querySelector("button");
 
+//using document.body.style.background
+
+// button.addEventListener("click", function(){
+
+// 	if(isPurple){
+// 		console.log("yes");
+// 		document.body.style.background = "white"
+// 		isPurple = false;
+// 	} else {
+// 		console.log("no");
+// 		document.body.style.background = "purple";
+// 		isPurple = true;
+// 	}
+// })
+
+// Using classlist.toggle
+
+
 button.addEventListener("click", function(){
 
 body.classList.toggle("color-change");
 
 })
+
+
+
