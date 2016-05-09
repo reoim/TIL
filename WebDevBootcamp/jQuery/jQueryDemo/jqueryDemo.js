2227e65ba35888c5f2d6b776fc8f026224ef6111
Dongmin (Reo) Lee <reoim10@gmail.com>
2016-05-10 01:50:05 +1000

    jQuery exercise

---
+++ WebDevBootcamp/jQuery/jQueryDemo/jqueryDemo.js
@@ -0,0 +1,8 @@
+//Select all divs and give them a purple backgorund
+$("div").css("background", "purple");
+//Select the divs with calss "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+//Select the div with id "third" and give it a orange border
+$("#third").css("border", "4px orange solid");
+//Select the first div only and change its font color to pink
+document.querySelectorAll("div")[0].style.color="pink";
