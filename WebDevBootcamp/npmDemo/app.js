bd5652cf9e57567484980956d769553ad81cafa8
Dongmin (Reo) Lee <reoim10@gmail.com>
2016-05-19 01:11:11 +1000

    npm exercise

---
+++ WebDevBootcamp/npmDemo/app.js
@@ -0,0 +1,4 @@
+var cat = require("cat-me");
+var knock = require("knock-knock-jokes");
+console.log(cat());
+console.log(knock());
---
+++ WebDevBootcamp/npmDemo/note.md
@@ -0,0 +1,4 @@
+#Installing and Using NPM
+
+* Use 'npm install' to install a package
+* Use 'require()' to include a package
