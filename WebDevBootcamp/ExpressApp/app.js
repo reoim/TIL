b9057af33427d3494ce493522b35c2a7d5905c0e
Dongmin (Reo) Lee <reoim10@gmail.com>
2016-05-20 01:47:01 +1000

    Route params

--- WebDevBootcamp/ExpressApp/app.js
+++ WebDevBootcamp/ExpressApp/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+
+
+
+
 // "/" => "Hi there!"
 app.get("/", function(req, res){
   res.send("Hi there!");
@@ -17,6 +21,17 @@ app.get("/dog", function(req, res){
   console.log("Someone request /dog");
   res.send("MEOW!");
 });
+
+app.get("/r/:subredditName", function(req, res){
+  console.log(req.params.subredditName);
+  res.send("You are in subredditName page!");
+});
+
+app.get("*", function(req, res){
+  res.send("You are a star!!");
+});
+
+
 
 app.listen(3000, function(){
   console.log('Example app listening on port 3000');
