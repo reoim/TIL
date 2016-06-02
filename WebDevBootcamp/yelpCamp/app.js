dbc5a33be7e742fdc9a5a3841b03f269f27f448e
Dongmin Lee <reoim10@gmail.com>
2016-06-03 01:50:15 +1000

    Basic layout added

--- WebDevBootcamp/yelpCamp/app.js
+++ WebDevBootcamp/yelpCamp/app.js
@@ -1,21 +1,37 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+  {name: "Salmon creek", image: "http://d3tgwe5x8vy0gu.cloudfront.net/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
+  {name: "Sunnybank", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdYM5TAPf0r9SgGnWTys8GUaJ--XijnoSXCUcxCcv146oMWSGEg"},
+  {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"}
+];
 
 app.get("/", function(req, res){
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-  var campgrounds = [
-    {name: "Salmon creek", image: "http://d3tgwe5x8vy0gu.cloudfront.net/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
-    {name: "Sunnybank", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdYM5TAPf0r9SgGnWTys8GUaJ--XijnoSXCUcxCcv146oMWSGEg"},
-    {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"}
-  ];
+
 
   res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name, image:image};
+  campgrounds.push(newCampground);
+
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
 });
 
 app.listen(3000, function(){
