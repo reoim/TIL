d4f2608297f9328b8b34238e7affb70a741d5a2d
Dongmin Lee <reoim10@gmail.com>
2016-06-04 01:31:29 +1000

    minor typo fixed

--- WebDevBootcamp/yelpCamp/app.js
+++ WebDevBootcamp/yelpCamp/app.js
@@ -6,9 +6,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
+
+  {name: "Sunnybank", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdYM5TAPf0r9SgGnWTys8GUaJ--XijnoSXCUcxCcv146oMWSGEg"},
+  {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"},
+  {name: "Mountain Gravatte", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAsJ3BrCoNyeaHh_0GPBYHB7nCqlRidK1IRa1a_IYREoVQ0PFB"},
   {name: "Salmon creek", image: "http://d3tgwe5x8vy0gu.cloudfront.net/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
   {name: "Sunnybank", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdYM5TAPf0r9SgGnWTys8GUaJ--XijnoSXCUcxCcv146oMWSGEg"},
-  {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"}
+  {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"},
+  {name: "Mountain Gravatte", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAsJ3BrCoNyeaHh_0GPBYHB7nCqlRidK1IRa1a_IYREoVQ0PFB"},
+  {name: "Salmon creek", image: "http://d3tgwe5x8vy0gu.cloudfront.net/~/media/DF58734103EF43669F1005AF8B668209.ashx"},
+  {name: "Sunnybank", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUdYM5TAPf0r9SgGnWTys8GUaJ--XijnoSXCUcxCcv146oMWSGEg"},
+  {name: "Bittangabe", image: "http://www.nationalparks.nsw.gov.au/~/media/D37B15D23FE74F8286DBF29D409E6520.ashx"},
+  {name: "Mountain Gravatte", image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSAsJ3BrCoNyeaHh_0GPBYHB7nCqlRidK1IRa1a_IYREoVQ0PFB"},
+  {name: "Salmon creek", image: "http://d3tgwe5x8vy0gu.cloudfront.net/~/media/DF58734103EF43669F1005AF8B668209.ashx"}
+
+
+
 ];
 
 app.get("/", function(req, res){
